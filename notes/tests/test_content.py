27dafe47453eb7461ee48c5b33f0fe4df939c471
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-05-18 15:40:19 +0300

    Черновик

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -15,12 +15,21 @@ from notes.forms import NoteForm
 User = get_user_model()
 
 
+
+#  Необходимо доделать работу!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Что в контексте лежит правильный ключ
+# 2. Что выводится на страницу тоже число заметок, что и создана
+# 3. Что страница используется правильный шаблон
+# 4. Что страница открывается гостем с нужным кодом ответа
+
+
 class TestHomePage(TestCase):
     # Вынесем ссылку на домашнюю страницу в атрибуты класса.
     LIST_URL = reverse('notes:list')
 
     @classmethod
-    def setUpTestData(cls):  # Групповое создание объектов (11 шт).
+    def setUpTestData(cls):  # Групповое создание объектов (10 шт).
         # Создаём пользователя:
         cls.author = User.objects.create(username='Александр Бунин')
         cls.all_note = [
@@ -30,17 +39,11 @@ class TestHomePage(TestCase):
                 slug=f'nov{index}',
                 author_id=1
             )
-            for index in range(10)
+            for index in range(11)
         ]
         Note.objects.bulk_create(cls.all_note)
 
-    def test_note_count(self):  # Проверяем количество объектов на странице.
-        # Загружаем страницу с заметками.
-        response = self.client.get(self.LIST_URL)
-        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
-        # Получаем список объектов из словаря контекста.
-        object_list = response.context['object_list']
-        # Определяем количество записей в списке.
-        note_count = object_list.count()
-        # Проверяем, что на странице именно 10 новостей.
-        self.assertEqual(note_count, 10)
+    def test_notes_count(self):
+        # Проверяем количество заметок на странице
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 10)
---
+++ notes/tests/test_logic.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+# Импортируем из файла с формами список стоп-слов и предупреждение формы.
+# Загляните в news/forms.py, разберитесь с их назначением.
+from notes.forms import NoteForm
+from notes.models import Note
+
+User = get_user_model()
+
+# ДОДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!
