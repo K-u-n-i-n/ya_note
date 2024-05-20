4053a551221c67c93dff5cbfa4523163c9032490
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-05-20 11:47:49 +0300

    Контент готов

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,4 +1,3 @@
-from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -22,6 +21,8 @@ class NotesFormsTests(TestCase):
             author=cls.author,
         )
 
+    # Тест для проверки, что в список заметок одного пользователя
+    # не попадают заметки другого пользователя.
     def test_notes_list_for_different_users(self):
         users_statuses = (
             (self.author, True),
@@ -35,6 +36,8 @@ class NotesFormsTests(TestCase):
             with self.subTest(user=user):
                 self.assertEqual((self.note in object_list), note_in_list)
 
+    # Тест для проверки, что на страницы создания и редактирования
+    # заметки передаются формы.
     def test_pages_contains_form(self):
         urls_args = (
             ('notes:add', None),
@@ -47,53 +50,3 @@ class NotesFormsTests(TestCase):
             with self.subTest(name=name):
                 self.assertIn('form', response.context)
                 self.assertIsInstance(response.context['form'], NoteForm)
-
-
-# from django.conf import settings
-# from django.test import TestCase
-# # Импортируем функцию для получения модели пользователя.
-# from django.contrib.auth import get_user_model
-# # Импортируем функцию reverse(), она понадобится для получения адреса страницы.
-# from django.urls import reverse
-
-
-# from notes.models import Note
-
-# # Импортируем класс формы.
-# from notes.forms import NoteForm
-
-
-# User = get_user_model()
-
-
-# #  Необходимо доделать работу!!!!!!!!!!!!!!!!!!!!!!!!
-
-# # Что в контексте лежит правильный ключ
-# # 2. Что выводится на страницу тоже число заметок, что и создана
-# # 3. Что страница используется правильный шаблон
-# # 4. Что страница открывается гостем с нужным кодом ответа
-
-
-# class TestHomePage(TestCase):
-#     # Вынесем ссылку на домашнюю страницу в атрибуты класса.
-#     LIST_URL = reverse('notes:list')
-
-#     @classmethod
-#     def setUpTestData(cls):  # Групповое создание объектов (10 шт).
-#         # Создаём пользователя:
-#         cls.author = User.objects.create(username='Александр Бунин')
-#         cls.all_note = [
-#             Note(
-#                 title=f'Новость {index}',
-#                 text='Текст',
-#                 slug=f'nov{index}',
-#                 author_id=1
-#             )
-#             for index in range(11)
-#         ]
-#         Note.objects.bulk_create(cls.all_note)
-
-#     def test_notes_count(self):
-#         # Проверяем количество заметок на странице
-#         notes_count = Note.objects.count()
-#         self.assertEqual(notes_count, 10)
